e0eac447db8103246336e635b328256eb1862815
TheFirstMe <sourabhsubhod@gmail.com>
2018-03-06 15:00:46 +0530

    components

--- src/js/app/app.module.js
+++ src/js/app/app.module.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('try',['blogList']);  
+angular.module('try',['blogList']);  //name of your app and included modules
--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,14 +1,27 @@
 'use strict';
 
-angular.module('blogList').
-    controller('blogListController', function($scope){
-        console.log("Hello")
-        $scope.title = 'Hi there'
-        $scope.clicks = 0
-        $scope.someClickTest = function(){
-            console.log("clicked")
-            $scope.clicks+=1
-            $scope.title = "Clicked " + $scope.clicks + " time(s)"
+angular.module('blogList').    //name of your module
+    component('blogList',{     //name of your custom tag with dashes in the middle ;)
+        template : "<div class=''><h1 class='new-class'> {{ title }} </h1><button ng-click='someClickTest()'>Click Me!</button></div>",
+        controller : function($scope){
+            console.log("Hello")
+            $scope.title = 'Hi there'
+            $scope.clicks = 0
+            $scope.someClickTest = function(){
+                console.log("clicked")
+                $scope.clicks+=1
+                $scope.title = "Clicked " + $scope.clicks + " time(s)"
+            }
         }
     });
+    // controller('blogListController', function($scope){
+    //     console.log("Hello")
+    //     $scope.title = 'Hi there'
+    //     $scope.clicks = 0
+    //     $scope.someClickTest = function(){
+    //         console.log("clicked")
+    //         $scope.clicks+=1
+    //         $scope.title = "Clicked " + $scope.clicks + " time(s)"
+    //     }
+    // });
     //component('blogList')
