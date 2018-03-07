d3cbf3d062ea556a6d0b0449b80adebd55c8417e
TheFirstMe <sourabhsubhod@gmail.com>
2018-03-07 21:48:02 +0530

    foreach loop

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,7 +3,7 @@
 angular.module('blogDetail').    //name of your module
     component('blogDetail',{     //name of your custom tag with dashes in the middle ;)
         templateUrl:'/templates/blog-detail.html',
-        controller : function($routeParams, $scope){
+        controller : function($location, $routeParams, $scope){
             var blogItems = [
                 {title : "Some title", id : 1, description : "This is a book."},
                 {title : "title", id : 2, description : "This is a book."},
@@ -13,5 +13,18 @@ angular.module('blogDetail').    //name of your module
 
             //console.log($routeParams.id)
             $scope.title = "Blog " + $routeParams.id
+            $scope.notFound = true
+            angular.forEach(blogItems, function(post){
+                if(post.id == $routeParams.id){
+                    $scope.notFound = false
+                    $scope.post = post
+                }
+            })
+
+            if($scope.notFound){
+                console.log("Not found")
+                //change location
+                $location.path("/")
+            }
         }
     });
